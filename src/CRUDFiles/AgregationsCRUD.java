81ce448af898af55738ab6e1a04fefb0327c39b9
21203729 <steven.essam.edwar@gmail.com>
2024-02-01 21:50:38 +0100

    fix

--- src/CRUDFiles/AgregationsCRUD.java
+++ src/CRUDFiles/AgregationsCRUD.java
@@ -5,7 +5,6 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Accumulators;
 import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.Projections;
 
 import org.bson.Document;
 import org.bson.conversions.Bson;
